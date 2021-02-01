5825944e8f4d92a08982f057a7f78fc9e290c5db
JJcompany <wc_s-p@daum.net>
2021-02-01 12:12:22 +0900

    Update Bluring.py
    change save and delete feature

--- Bluring.py
+++ Bluring.py
@@ -1,46 +1,54 @@
 import cv2
 import glob
 import win32api
-#import numpy as np
+import os.path
+
 from ctypes import windll
 
+indexFileName = 'index.txt'
+is_bluring = False
+is_change = False #이미지 블러 유무 체크
 
-
-abc = False
+indexFile = open(indexFileName,'r')
 images = glob.glob('./before/*.png')
+#afterImages = glob.glob('./after/*.png')
+afterImages = indexFile.readline()
+if afterImages == 'e':#끝났었을 경우 다시 0으로 초기화
+    afterImages = 0
+
 imageIndex = 0
 img = ''
 pointSize = 15
 wname = 'Bluring'
 cv2.namedWindow(wname)
 hicon = ""
+#print(images)
+
+def saveIndex(index):
+    global indexFileName
+    indexFile = open(indexFileName,'w')
+    indexFile.write(str(index))
+    indexFile.close()
+
 
 def draw_eraser(event, x, y, flags, param):
-    global abc
-    global img
+    global is_bluring, is_change, img
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        abc = True
+        is_bluring = True
 
     elif event == cv2.EVENT_MOUSEMOVE:
-        if (abc == True):
+        if (is_bluring == True):
             blur = img[y:y + pointSize, x:x + pointSize]
-            blur = cv2.GaussianBlur(blur, (23, 23), 50)
+            blur = cv2.GaussianBlur(blur, (3,3), 0)
             img[y:y + blur.shape[0], x:x + blur.shape[1]] = blur
+            is_change = True
     else:
-        abc = False
-'''
-def hangulFilePathImageRead ( filePath ) :
-    stream = open( filePath.encode("utf-8") , "rb")
-    bytes = bytearray(stream.read())
-    numpyArray = np.asarray(bytes, dtype=np.uint8)
-
-    return cv2.imdecode(numpyArray , cv2.IMREAD_UNCHANGED)
-'''
-
+        is_bluring = False
 
 def createCursor():
     global hicon
+
     CreateIconFromResourceEx = windll.user32.CreateIconFromResourceEx
     size_x, size_y = pointSize, pointSize
     LR_DEFAULTCOLOR = 0
@@ -54,48 +62,212 @@ def createCursor():
 def showCursor():
     win32api.SetCursor(hicon)  # change cursor
 
-createCursor()
+def showImage(index):
+    global wname,img, is_change
 
-
-if len(images) != 0:
-    img = cv2.imread(images[imageIndex])
-    cv2.setMouseCallback(wname, draw_eraser)
+    is_change = False
+    img = cv2.imread(images[index])
+    cv2.imshow(wname, img)
     print(images[imageIndex])
 
+createCursor()
 
-while True:
-    showCursor()
-    cv2.imshow(wname, img)
+if len(images) != 0 and afterImages != len(images):
+    cv2.setMouseCallback(wname, draw_eraser)
 
-    c = cv2.waitKey(1)
+    imageIndex = int(afterImages)
+    #if len(afterImages) != 0:
+        #imageIndex = len(afterImages)
 
-    if c == ord('p'):
-        break
-    elif c == ord('n'):
-        #기존 이미지 저장
-        imageName = images[imageIndex].split('\\')
-        cv2.imwrite('./after/'+imageName[len(imageName)-1],img)
+    saveIndex(imageIndex)
+    showImage(imageIndex)#최초 이미지 로딩
 
-        #새 이미지 불러오기
-        imageIndex = imageIndex + 1
-        if imageIndex >= len(images):
-            break
-        img = cv2.imread(images[imageIndex])
-        #img = hangulFilePathImageRead(images[imageIndex])
+
+    while True:
+        showCursor()
         cv2.imshow(wname, img)
-        #cv2.imshow(wname,"23_160577294815101520044-복사본.jpg")
-        print(images[imageIndex])
 
+        c = cv2.waitKey(1)
 
-    elif c == 44:
-        if pointSize > 5:
-            pointSize = pointSize-1
-            createCursor()
+        if c == ord('p'):#프로그램 중지
+            break
+        elif c == ord('1'):#이미지 저장 + 다음 이미지 불러옫기
+            #기존 이미지 저장
+            imageName = images[imageIndex].split('\\')
+            imageName = imageName[len(imageName)-1]
 
-    elif c == 46:
-        if pointSize < 100:
-            pointSize = pointSize+1
-            createCursor()
+            if is_change:
+                imageName = imageName.replace("_N_","_E_")#실제 파일명 뽑기
 
+            cv2.imwrite('./1/'+imageName,img)
+
+            #새 이미지 불러오기
+            imageIndex = imageIndex + 1
+            if imageIndex >= len(images):
+                saveIndex('e')
+                break
+
+            saveIndex(imageIndex)
+            showImage(imageIndex)
+        elif c == ord('2'):
+            #기존 이미지 저장
+            imageName = images[imageIndex].split('\\')
+            imageName = imageName[len(imageName)-1]
+
+            if is_change:
+                imageName = imageName.replace("_N_","_E_")#실제 파일명 뽑기
+
+            cv2.imwrite('./2/'+imageName,img)
+
+            #새 이미지 불러오기
+            imageIndex = imageIndex + 1
+            if imageIndex >= len(images):
+                saveIndex('e')
+                break
+
+            saveIndex(imageIndex)
+            showImage(imageIndex)
+        elif c == ord('3'):
+            #기존 이미지 저장
+            imageName = images[imageIndex].split('\\')
+            imageName = imageName[len(imageName)-1]
+
+            if is_change:
+                imageName = imageName.replace("_N_","_E_")#실제 파일명 뽑기
+
+            cv2.imwrite('./3/'+imageName,img)
+
+            #새 이미지 불러오기
+            imageIndex = imageIndex + 1
+            if imageIndex >= len(images):
+                saveIndex('e')
+                break
+
+            saveIndex(imageIndex)
+            showImage(imageIndex)
+        elif c == ord('4'):
+            #기존 이미지 저장
+            imageName = images[imageIndex].split('\\')
+            imageName = imageName[len(imageName)-1]
+
+            if is_change:
+                imageName = imageName.replace("_N_","_E_")#실제 파일명 뽑기
+
+            cv2.imwrite('./4/'+imageName,img)
+
+            #새 이미지 불러오기
+            imageIndex = imageIndex + 1
+            if imageIndex >= len(images):
+                saveIndex('e')
+                break
+
+            saveIndex(imageIndex)
+            showImage(imageIndex)
+        elif c == ord('5'):
+            #기존 이미지 저장
+            imageName = images[imageIndex].split('\\')
+            imageName = imageName[len(imageName)-1]
+
+            if is_change:
+                imageName = imageName.replace("_N_","_E_")#실제 파일명 뽑기
+
+            cv2.imwrite('./5/'+imageName,img)
+
+            #새 이미지 불러오기
+            imageIndex = imageIndex + 1
+            if imageIndex >= len(images):
+                saveIndex('e')
+                break
+
+            saveIndex(imageIndex)
+            showImage(imageIndex)
+        elif c == ord('6'):
+            #기존 이미지 저장
+            imageName = images[imageIndex].split('\\')
+            imageName = imageName[len(imageName)-1]
+
+            if is_change:
+                imageName = imageName.replace("_N_","_E_")#실제 파일명 뽑기
+
+            cv2.imwrite('./6/'+imageName,img)
+
+            #새 이미지 불러오기
+            imageIndex = imageIndex + 1
+            if imageIndex >= len(images):
+                saveIndex('e')
+                break
+
+            saveIndex(imageIndex)
+            showImage(imageIndex)
+        elif c == ord('b'):#이전 이미지 불러오기
+            imageIndex = imageIndex -1
+            if imageIndex < 0:
+                saveIndex(0)
+                break
+            else :
+                saveIndex(imageIndex)
+                showImage(imageIndex)
+                delName = images[imageIndex].split('\\')
+                delName = delName[len(delName)-1]
+                delName2 = delName.replace("_N_", "_E_")#다른 파일명 뽑기
+
+                if os.path.isfile('./1/'+delName):#파일이 있는 경우
+                    os.remove('./1/'+delName)
+                    print('./1/'+delName)
+                elif os.path.isfile('./1/'+delName2):
+                    os.remove('./1/'+delName2)
+                    print('./1/'+delName2)
+                elif os.path.isfile('./2/'+delName):
+                    os.remove('./2/'+delName)
+                    print('./2/'+delName)
+                elif os.path.isfile('./2/' + delName2):
+                    os.remove('./2/' + delName2)
+                    print('./2/' + delName2)
+                elif os.path.isfile('./3/' + delName):
+                    os.remove('./3/' + delName)
+                    print('./3/' + delName)
+                elif os.path.isfile('./3/' + delName2):
+                    os.remove('./3/' + delName2)
+                    print('./3/' + delName2)
+                elif os.path.isfile('./4/' + delName):
+                    os.remove('./4/' + delName)
+                    print('./4/' + delName)
+                elif os.path.isfile('./4/' + delName2):
+                    os.remove('./4/' + delName2)
+                    print('./4/' + delName2)
+                elif os.path.isfile('./5/' + delName):
+                    os.remove('./5/' + delName)
+                    print('./5/' + delName)
+                elif os.path.isfile('./5/' + delName2):
+                    os.remove('./5/' + delName2)
+                    print('./5/' + delName2)
+                elif os.path.isfile('./6/' + delName):
+                    os.remove('./6/' + delName)
+                    print('./6/' + delName)
+                elif os.path.isfile('./6/' + delName2):
+                    os.remove('./6/' + delName2)
+                    print('./6/' + delName2)
+                        
+        elif c == ord('j'):#다음 이미지 불러오기 + 이미지 저장 없음
+            #새 이미지 불러오기
+            imageIndex = imageIndex + 1
+            if imageIndex >= len(images):
+                saveIndex('e')
+                break
+            saveIndex(imageIndex)
+            showImage(imageIndex)
+            
+        elif c == ord('r'):#이미지 리로드
+            showImage(imageIndex)
+            
+        elif c == 44:#포인터 사이즈 다운
+            if pointSize > 5:
+                pointSize = pointSize-1
+                createCursor()
+
+        elif c == 46:#포인터 사이즈 업
+            if pointSize < 100:
+                pointSize = pointSize+1
+                createCursor()
 
 cv2.destroyAllWindows()
